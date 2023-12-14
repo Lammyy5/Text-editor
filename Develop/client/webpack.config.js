6a22409b480201b0c0fb6c10adfd1dbcba3d34a2
Lammyy5 <reliford2010@gmail.com>
2023-12-13 16:51:34 -0800

    added asset caching and plugins

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -8,23 +8,28 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+		mode: 'development',
+		entry: {
+			main: './src/js/index.js',
+			install: './src/js/install.js',
+		},
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		plugins: [
+			new InjectManifest({
+				swSrc: './src-sw.js',
+				swDest: 'src-sw.js',
+			}),
+			new HtmlWebpackPlugin({
+				template: './index.html',
+				title: 'Just Another Text Editor',
+			}),
+		],
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+		module: {
+			rules: [],
+		},
+	}
 };
